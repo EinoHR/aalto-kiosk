dda9c0f35b9e52b4a5c5129024974e1739825944
EinoHR <rauhalaeino@gmail.com>
2023-02-20 15:38:45 +0200

    don't put java in the background

--- mycelium/mycelium.sh
+++ mycelium/mycelium.sh
@@ -1,3 +1,3 @@
 cd /usr/mycelium
 ./dbclient &
-/usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar &
+/usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar
