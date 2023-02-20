869c398bad16b04d64077caf0815c5abaa58cc92
EinoHR <rauhalaeino@gmail.com>
2023-02-20 16:06:45 +0200

    crash handling for mycelium.sh

--- mycelium/mycelium.sh
+++ mycelium/mycelium.sh
@@ -1,3 +1,13 @@
 cd /usr/mycelium
-./dbclient &
-/usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar
+
+(
+    until ./dbclient; do
+        echo "Dbclient crashed with exit code $?.  Respawning.." >&2
+        sleep 1
+    done
+)&
+
+until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar; do
+    echo "Java crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
