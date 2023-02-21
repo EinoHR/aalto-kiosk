15c1a5c8512e9b37b266a730d51d7ffd384faebe
EinoHR <rauhalaeino@gmail.com>
2023-02-21 13:59:16 +0200

    arg in wrong place?

--- mycelium/mycelium.sh
+++ mycelium/mycelium.sh
@@ -8,7 +8,7 @@ cd /usr/mycelium
         done
     )&
 
-    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar -Djava.io.tmpdir=/var/mycelium; do
+    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -Djava.io.tmpdir=/var/mycelium -jar mycelium.jar; do
         echo "Java crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
