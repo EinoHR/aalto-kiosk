45b6bd78c5723c431e42800c76c8dce2593b7869
EinoHR <rauhalaeino@gmail.com>
2023-02-21 11:28:49 +0200

    try logging stdout & stderr

--- mycelium/mycelium.sh
+++ mycelium/mycelium.sh
@@ -1,13 +1,15 @@
 cd /usr/mycelium
 
 (
-    until ./dbclient; do
-        echo "Dbclient crashed with exit code $?.  Respawning.." >&2
+    (
+        until ./dbclient; do
+            echo "Dbclient crashed with exit code $?.  Respawning.." >&2
+            sleep 1
+        done
+    )&
+
+    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar; do
+        echo "Java crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
-)&
-
-until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar; do
-    echo "Java crashed with exit code $?.  Respawning.." >&2
-    sleep 1
-done
+) &> /var/log/mycelium.log
