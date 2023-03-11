e9ebba3bf5f3de5259151f16c5c86cb132fe2084
EinoHR <rauhalaeino@gmail.com>
2023-03-11 12:28:20 +0200

    script-level logging

--- myceliumupdate.sh
+++ myceliumupdate.sh
@@ -1,16 +1,18 @@
-cd ~/
+(
+    cd ~/
 
-if [ -f "mycelium" ]; then
-    echo "Folder 'mycelium' already exists, continuing."
-else
-    git clone https://github.com/EinoHR/mycelium
-fi
+    if [ -f "mycelium" ]; then
+        echo "Folder 'mycelium' already exists, continuing."
+    else
+        git clone https://github.com/EinoHR/mycelium
+    fi
 
-cd mycelium
-git fetch --all
-git reset --hard origin/main
+    cd mycelium
+    git fetch --all
+    git reset --hard origin/main
 
-chmod +x mycelium.sh
-chmod +x dbclient
+    chmod +x mycelium.sh
+    chmod +x dbclient
+) &> /var/home/mycelium/myceliumupdate.log
 
 ./mycelium.sh
