08f511f4cf98734c27822b236fd8d885aa95974b
EinoHR <rauhalaeino@gmail.com>
2023-03-11 13:10:13 +0200

    some file path tweaks

--- myceliumupdate.sh
+++ myceliumupdate.sh
@@ -7,12 +7,12 @@
         git clone https://github.com/EinoHR/mycelium
     fi
 
-    cd mycelium
+    cd ./mycelium
     git fetch --all
     git reset --hard origin/main
 
-    chmod +x mycelium.sh
-    chmod +x dbclient
+    chmod +x ./mycelium.sh
+    chmod +x ./dbclient
 
     ./mycelium.sh
 ) &> /var/home/mycelium/myceliumupdate.log
